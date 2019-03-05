a08aefcb20ad21af272e9dd4559729a4f9ef1778
amanrajdce <amanrajdce@gmail.com>
2019-03-05 03:51:12 -0800

    changed paths

--- run_training_kitti_pod.sh
+++ run_training_kitti_pod.sh
@@ -1,14 +1,14 @@
-ckpt_dir="/viscompfs/users/amanraj/struct2depth/training/scratch_imgnetckpt"
+ckpt_dir="/viscompfs/users/amanraj/data/experiment/training/scratch_imgnetckpt"
 data_dir="/viscompfs/users/amanraj/data/kitti_processed/" # Set for KITTI
 seg_data_dir="/viscompfs/users/amanraj/data/kitti_eigen_instance/" # Set for KITTI
-imagenet_ckpt="/viscompfs/users/amanraj/struct2depth/resnet_pretrained/model.ckpt"
-pretrained_ckpt="/viscompfs/users/amanraj/struct2depth/models/model"
+imagenet_ckpt="/viscompfs/users/amanraj/data/experiment/resnet_pretrained/model.ckpt"
+pretrained_ckpt="/viscompfs/users/amanraj/data/experiment/checkpoint/model"
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
   --logtostderr \
   --checkpoint_dir $ckpt_dir \
   --data_dir $data_dir \
-  --seg_data_dir $seg_data_dir \
+  --ins_data_dir $ins_data_dir \
   --architecture resnet \
   --imagenet_ckpt $imagenet_ckpt \
   --batch_size 4 \
